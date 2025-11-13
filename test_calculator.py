eb9f2058227b151ffbca8c06b152e58e295d35e0
woolly423 <its.spaghetti.man1219@gmail.com>
2025-11-12 19:55:22 -0500

    final

--- test_calculator.py
+++ test_calculator.py
@@ -8,16 +8,16 @@ class TestCalculator(unittest.TestCase):
     #     fill in code
     def test_add(self):
         self.assertEqual(add(1,0),1)
-        self.assertEqual(add(-1,1.1),0.1)
+        self.assertEqual(add(-1,1),0.)
         self.assertEqual(add(66,1),67)
         
     # def test_subtract(self): # 3 assertions
     #     fill in code
     # ##########################
     def test_subtract(self):
-        self.assertEqual(sub(1,0),1)
-        self.assertEqual(sub(5.5,10),-4.5)
-        self.assertEqual(sub(68,1),67)
+        self.assertEqual(subtract(1,0),1)
+        self.assertEqual(subtract(5.5,10),-4.5)
+        self.assertEqual(subtract(68,1),67)
 
     ####### Partner 1
     def test_multiply(self): # 3 assertions
@@ -43,9 +43,9 @@ class TestCalculator(unittest.TestCase):
     # def test_logarithm(self): # 3 assertions
     #     fill in code
     def test_logarithm(self):
-        self.assertEqual(log(2,8),3)
-        self.assertEqual(log(4,256),4)
-        self.assertEqual(log(10,100),2)
+        self.assertEqual(logarithm(2,8),3)
+        self.assertEqual(logarithm(4,256),4)
+        self.assertEqual(logarithm(10,100),2)
 
     # def test_log_invalid_base(self): # 1 assertion
     #     # use same technique from test_divide_by_zero
